ea1e0d24489e2cd44e2134627105097e6121609c
sugathb <sugathb@gmail.com>
2021-08-06 00:11:11 +1000

    initial-draft-a[i-project

---
+++ MusicStore.Api/MusicStore.Api/Controllers/MusicController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MusicStore.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MusicController : ControllerBase
+    {
+       private readonly ILogger<MusicController> _logger;
+
+        public MusicController(ILogger<MusicController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello");
+        }
+    }
+}
