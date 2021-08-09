058b3b7c6f622b7cba38a2795e792236039f103c
sugathb <sugathb@gmail.com>
2021-08-10 00:23:08 +1000

    music-store-db-context-added

---
+++ MusicStore.Api/MusicStore.Infrastructure/Data/MusicStoreDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicStore.Domain;
+
+namespace MusicStore.Infrastructure.Data
+{
+    public class MusicStoreDbContext : DbContext
+    {
+        public MusicStoreDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public virtual DbSet<Artist> Artists { get; set; }
+        public virtual DbSet<Album> Albums { get; set; }
+        public virtual DbSet<Genre> Genres { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Artist>(entity =>
+            {
+                entity.HasKey(e => e.ArtistId);
+                entity.Property(e => e.Name).HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<Album>(entity =>
+            {
+                entity.HasKey(e => e.AlbumId);
+            });
+
+            modelBuilder.Entity<Genre>(entity =>
+            {
+                entity.HasKey(e => e.GenreId);
+            });
+        }
+    }
+}
