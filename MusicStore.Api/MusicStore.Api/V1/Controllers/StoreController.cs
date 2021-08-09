705331367f16e7d31d4bdfb847f62e0015b5f6b9
sugathb <sugathb@gmail.com>
2021-08-10 00:14:03 +1000

    renamed-controller

---
+++ MusicStore.Api/MusicStore.Api/V1/Controllers/StoreController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MusicStore.Api.V1.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class StoreController : ControllerBase
+    {
+       private readonly ILogger<StoreController> _logger;
+
+        public StoreController(ILogger<StoreController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Browse(string genre)
+        {
+            return Ok("Hello");
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            return Ok("Hello");
+        }
+    }
+}
